f57904ebb4d03e834e374883d22ef18b693e37f4
swathimaku <swathi.m16@gmail.com>
2022-06-06 15:50:21 +0530

    fromgitbascheckin

--- ECommerce.Website/Default.aspx.cs
+++ ECommerce.Website/Default.aspx.cs
@@ -11,7 +11,10 @@ namespace ECommerce.Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(IsPostBack)
+            {
 
+            }
         }
     }
 }
